8e0bfd533006432b7ca71dbb39924de0564cdacf
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2025-01-30 18:25:40 +1100

    google auth over discord

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -5,7 +5,7 @@ import type {
 } from "next-auth";
 import { skipCSRFCheck } from "@auth/core";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import Discord from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "@acme/db/client";
 import { Account, Session, User } from "@acme/db/schema";
@@ -38,7 +38,12 @@ export const authConfig = {
       }
     : {}),
   secret: env.AUTH_SECRET,
-  providers: [Discord],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
   callbacks: {
     session: ({ session }) => {
       return session;
